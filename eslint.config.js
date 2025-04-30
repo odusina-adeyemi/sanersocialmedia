532f9a283189be204e7da0750cde60bd225fa710
Tobias Dalhof <dalhof.tobias@gmail.com>
2025-04-30 14:22:57 +0200

    Update README.md

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,9 @@
 import antfu from '@antfu/eslint-config'
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 export default antfu({
   unocss: true,
   vue: true,
-})
+});
